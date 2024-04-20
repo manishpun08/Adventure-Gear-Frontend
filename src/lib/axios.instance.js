6d5b8ea859a8392c24099ca92d92fd82541c7726
manishpun08 <manishpunmagar6@gmail.com>
2024-04-20 19:36:50 +0545

    axios resolve

--- src/lib/axios.instance.js
+++ src/lib/axios.instance.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-const $axios = axios.create({
-  baseURL: "http://localhost:8000",
-  timeout: 5000,
-});
-
 // const $axios = axios.create({
-//   baseURL: "https://long-tan-snapper-tutu.cyclic.app/",
+//   baseURL: "http://localhost:8000",
 //   timeout: 5000,
 // });
+
+const $axios = axios.create({
+  baseURL: "https://long-tan-snapper-tutu.cyclic.app/",
+  timeout: 5000,
+});
 
 // Add a request interceptor
 $axios.interceptors.request.use(function (config) {
